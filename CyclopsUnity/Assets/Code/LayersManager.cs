3a91baff51473c156b717447dc4af6869ee92c87
WUStudent <wustudent@webster.edu>
2023-06-28 15:49:27 -0500

    yes

---
+++ CyclopsUnity/Assets/Code/LayersManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayersManager : MonoBehaviour
+{
+    public List<GameObject> layers;
+
+    void Start()
+    {
+        ActivateLayer(0);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            ActivateLayer(0);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            ActivateLayer(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            ActivateLayer(2);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            ActivateLayer(3);
+        }
+    }
+
+    public void ActivateLayer(int index)
+    {
+        foreach (GameObject layer in layers)
+        {
+            layer.SetActive(false);
+        }
+
+        layers[index].SetActive(true);
+    }
+}
